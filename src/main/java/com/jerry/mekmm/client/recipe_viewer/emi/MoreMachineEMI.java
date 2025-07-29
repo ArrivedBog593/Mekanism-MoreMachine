2fef74f871062a43ed3382920f7aa5d556ab4b36
10565552046 <1056552046@qq.com>
2025-07-29 10:17:58 +0800

    adapt emi

---
+++ src/main/java/com/jerry/mekmm/client/recipe_viewer/emi/MoreMachineEMI.java
@@ -0,0 +1,14 @@
+package com.jerry.mekmm.client.recipe_viewer.emi;
+
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+
+@EmiEntrypoint
+public class MoreMachineEMI implements EmiPlugin {
+
+    @Override
+    public void register(EmiRegistry registry) {
+
+    }
+}
