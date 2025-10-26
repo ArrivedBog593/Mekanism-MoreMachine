6c29c7726c45ed8d050ad7c7918b226137467e65
10565552046 <1056552046@qq.com>
2025-10-26 15:37:58 +0800

    compatible with botania

---
+++ src/main/java/com/jerry/datagen/common/recipe/compat/BotaniaRecipeProvider.java
@@ -0,0 +1,19 @@
+package com.jerry.datagen.common.recipe.compat;
+
+import mekanism.api.annotations.ParametersAreNotNullByDefault;
+import net.minecraft.data.recipes.FinishedRecipe;
+
+import java.util.function.Consumer;
+
+@ParametersAreNotNullByDefault
+public class BotaniaRecipeProvider extends CompatRecipeProvider {
+
+    public BotaniaRecipeProvider(String modId) {
+        super(modId);
+    }
+
+    @Override
+    protected void registerRecipes(Consumer<FinishedRecipe> consumer, String basePath) {
+
+    }
+}
